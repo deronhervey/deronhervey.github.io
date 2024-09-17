c9cb94402997a9b38f12b5f0d2b3e4433fefb13c
deronhervey <deron.hervey@jpschools.org>
2024-09-17 15:26:51 +0000

    <added platformer>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,6 +38,11 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    createPlatform(0,550,450,30)
+    createPlatform(550,650,500,30)
+    createPlatform(1000,350,500,30)
+    createPlatform(550,435,220,30)
+    createPlatform(60,375,300,30)
 
 
 
@@ -47,6 +52,12 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable("max", 250, 0, 20, 0.8)
+    createCollectable("max", 650, 500, 20, 0.8)
+    createCollectable("max", 1000, 300, 20, 0.8)
+    createCollectable("max", 150, 600, 20, 0.8)
+    createCollectable("max", 1200, 0, 20, 0.8)
+
 
 
 
@@ -56,6 +67,10 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon("right", 600, 1000)
+    createCannon("right", 750, 1700)
+    createCannon("top", 550, 1000)
+    createCannon("right", 360, 1700)
 
 
 
